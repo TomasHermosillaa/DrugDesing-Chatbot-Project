f8c3a3cc30c5fc01d3e030374b75eedbb60f0764
TomasHermo <tomas.hermosilla.p@gmail.com>
2024-03-27 11:51:00 -0300

    Modular python code

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.1',
+    author= 'Tomás Hermosilla',
+    author_email= 'tomas.hermosilla.p@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
